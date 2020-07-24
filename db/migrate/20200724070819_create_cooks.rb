77165a032d7294dd1980654da4bc8926be62c608
Togo <a.kirigo.t0630@gmail.com>
2020-07-24 07:10:55 +0000

    [追加]モデル等

---
+++ db/migrate/20200724070819_create_cooks.rb
@@ -0,0 +1,8 @@
+class CreateCooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cooks do |t|
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/requests/cooks_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Cooks", type: :request do
+
+end
